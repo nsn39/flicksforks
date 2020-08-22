eeef259c74060f7c64be839af685afc280a1e7fd
NISHAN <heyitsnishan2020@gmail.com>
2020-08-22 09:26:28 +0545

    Deploying to Herokugit add -A :D

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://dmwnishan:tendamagic@cluster0-zimwi.mongodb.net/flicksforks?retryWrites=true&w=majority'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dmwnishan:tendamagic@cluster0-zimwi.mongodb.net/flicksforks?retryWrites=true&w=majority'
         , {useNewUrlParser: true, useUnifiedTopology:true}
     ).catch(e => {
         console.log('Error received: ', e);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyparser = require('body-parser');
 const cors = require('cors');
 const db = require('./db/index');
 const movieRouter = require('./routes/router');
+const path = require("path")
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,12 +19,26 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use('/api', movieRouter);
+
 
 //create a GET Route 
 app.get('/data', (req, res) => {
     res.send({message: "Hey you did it man!"});
 });
+
+// Right before your app.listen(), add this:
+/* app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+}); */
+
+
+if (process.env.NODE__ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
 // Listeing to the server
 app.listen(port, () => console.log('App is running on the port'));
